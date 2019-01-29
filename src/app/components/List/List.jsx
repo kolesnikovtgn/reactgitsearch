94ee4e08f93e5af74b843da0de17661e0f0f7c8e
Kolesnikov Vladimir <lodoss.kolesnikov@gmail.com>
2019-01-29 17:31:37 +0300

    Added change style block or line and render block or line repo

--- src/app/components/List/List.jsx
+++ src/app/components/List/List.jsx
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
 import './List.scss';
+import Block from '../Block/Block';
 import Line from '../Line/Line'
 
 class List extends Component {
+    constructor(props) {
+        super(props);
+        this.handleBlockClick = this.handleBlockClick.bind(this);
+        this.handleLineClick = this.handleLineClick.bind(this);
+        this.state = {isBlockType: true};
+    }
+
+    handleBlockClick() {
+        this.setState({isBlockType: true});
+    }
+
+    handleLineClick() {
+        this.setState({isBlockType: false});
+    }
+
     render() {
+        const isTypeProjects = this.state.isBlockType;
+
+        let renderType = null;
+
+        if (isTypeProjects) {
+            renderType = <Block></Block>
+        } else {
+            renderType = <Line></Line>
+        }
         return (
             <div className="mainList">
               <div className="mainList__menu">
-                <span className="mainList__menu-block">
+                <span className={"mainMyRecord__menu-block" + (isTypeProjects ? ' activeIcon': ' not-activeIcon')} onClick={this.handleBlockClick}>
                     <i className="fas fa-th-large fa-3x"></i>
                 </span>
-                <span className="mainList__menu-list">
+                <span className={"mainMyRecord__menu-list" + (isTypeProjects ? ' not-activeIcon': ' activeIcon')} onClick={this.handleLineClick}>
                     <i className="fas fa-bars fa-3x"></i>
                 </span>
               </div>
             <div className="mainList-projects">
-                <Line></Line>
-            </div>
-            <div className="mainList-projects">
-
+                {renderType}
             </div>
             </div>
 
--- src/app/components/MyRecord/MyRecord.jsx
+++ src/app/components/MyRecord/MyRecord.jsx
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
 import './MyRecord.scss';
-import Block from "../Block/Block";
+import Block from '../Block/Block';
+import Line from '../Line/Line'
 
 class MyRecord extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleBlockClick = this.handleBlockClick.bind(this);
+        this.handleLineClick = this.handleLineClick.bind(this);
+        this.state = {isBlockType: true};
+    }
+
+    handleBlockClick() {
+        this.setState({isBlockType: true});
+    }
+
+    handleLineClick() {
+        this.setState({isBlockType: false});
+    }
+
     render() {
+        const isTypeProjects = this.state.isBlockType;
+
+        let renderType = null;
+
+        if (isTypeProjects) {
+            renderType = <Block></Block>
+        } else {
+            renderType = <Line></Line>
+        }
+
         return (
             <div className="mainMyRecord">
                 <div className="mainMyRecord__menu">
-                <span className="mainMyRecord__menu-block">
+                <span className={"mainMyRecord__menu-block" + (isTypeProjects ? ' activeIcon': ' not-activeIcon')} onClick={this.handleBlockClick}>
                     <i className="fas fa-th-large fa-3x"></i>
                 </span>
-                    <span className="mainMyRecord__menu-list">
+                    <span className={"mainMyRecord__menu-list" + (isTypeProjects ? ' not-activeIcon': ' activeIcon')} onClick={this.handleLineClick}>
                     <i className="fas fa-bars fa-3x"></i>
                 </span>
                 </div>
                 <div className="mainMyRecord-projects">
-                    <Block></Block>
-                    <Block></Block>
-                </div>
-                <div className="mainMyRecord-projects">
-
+                    {renderType}
                 </div>
             </div>
 

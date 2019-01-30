30758397c734babade52eac8cd16f0285561297f
Kolesnikov Vladimir <lodoss.kolesnikov@gmail.com>
2019-01-30 11:56:10 +0300

    testing with redux

--- src/app/components/Search/Search.jsx
+++ src/app/components/Search/Search.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import './Search.scss';
 import MyRecord from '../MyRecord/MyRecord';
-import { connect } from  'F:\\lodoss\\reactgitsearch\\node_modules\\react-redux';
+// import { bindActionCreators } from '../../../../node_modules/redux';
+import { connect } from  '../../../../node_modules/react-redux';
 import { getRepositoriesAction } from '../../../store/action';
+// import * as pageActions from '../../../store/action';
 
 class Search extends Component {
+
+  // constructor(props) {
+  //   super(props);
+  //
+  //     this.state = {
+  //         repositories: []
+  //     };
+  // }
+
+  componentDidMount() {
+      // fetch(`https://api.github.com/search/repositories?q=cms+language:javascript`)
+      //     .then(response => response.json())
+      //     .then(data => { this.setState({ repositories: data.items }); console.log('this state is ', this.state); });
+
+  }
+
   searchRequest = () => {
+      console.log('before func', this.props);
     this.props.onGetRepositories();
+      console.log('after func', this.props);
   };
     render() {
+        // const { repositories } = this.state;
         return (
             <div className="mainSearch">
               <div className="search container-row">
@@ -25,6 +46,13 @@ class Search extends Component {
                   <button className="search__btn" onClick={this.searchRequest}><p className="search__btn-text">SEARCH</p></button>
                 </div>
               </div>
+                {/*<ul>*/}
+                    {/*{repositories.map(el =>*/}
+                        {/*<li key={el.id}>*/}
+                            {/*{el.name} {el.description} {el.myList}*/}
+                        {/*</li>*/}
+                    {/*)}*/}
+                {/*</ul>*/}
               {this.props.name}
               {this.props.description}
                 <MyRecord></MyRecord>
@@ -43,6 +71,12 @@ function mapStateToProps(state) {
     myList: state.myList
   }
 }
+
+// function mapDispatchToProps(dispatch) {
+//     return {
+//         pageActions: bindActionCreators(pageActions, dispatch)
+//     }
+// }
 
 function mapDispatchToProps(dispatch) {
   return {
--- src/store/action.js
+++ src/store/action.js
@@ -1,16 +1,26 @@
 export const GET_REPOSITORIES = 'GET_REPOSITORIES';
 
-export const getRepositoriesAction = () => dispatch => fetch({
-  // eslint-disable-line
-  method: 'GET',
-  url: `https://api.github.com/search/repositories?q=cms+language:javascript`,
-  headers: { Authorization: 'Basic -u kolesnikovtgn:93e92c2a74aa92e55cc5bfd9b3728901cf2258bb' },
-}).then((data) => {
+export const getRepositoriesAction = () => dispatch => fetch(`https://api.github.com/search/repositories?q=cms+language:javascript`)
+  .then((data) => {
   // data.forEach(el => el.myList = false);
-
   const userData = JSON.parse(JSON.stringify(data));
   dispatch({
     type: 'GET_REPOSITORIES',
     payload: userData,
   });
 });
+
+// export const getRepositoriesAction = () => dispatch => fetch({
+//     // eslint-disable-line
+//     method: 'GET',
+//     url: `https://api.github.com/search/repositories?q=cms+language:javascript`,
+//     headers: { Authorization: 'Basic -u kolesnikovtgn:93e92c2a74aa92e55cc5bfd9b3728901cf2258bb' },
+// }).then((data) => {
+//     // data.forEach(el => el.myList = false);
+//     console.log('my asyncData', data);
+//     const userData = JSON.parse(JSON.stringify(data));
+//     dispatch({
+//         type: 'GET_REPOSITORIES',
+//         payload: userData,
+//     });
+// });
